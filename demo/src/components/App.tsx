f74af4a6c2a1b357f09bc0d443b5a67c3e76378d
nariakiiwatani <nariakiiwatani@annolab.com>
2020-08-10 21:20:12 +0900

    edit package.json to prepare publishing

--- demo/src/components/App.tsx
+++ demo/src/components/App.tsx
@@ -2,8 +2,8 @@
 import React, { useState } from 'react'
 // This import is only for this demo to use the newest version.
 import { PlainJsonEditor } from '../../../dist/PlainJsonEditor'
-// you would do `npm i plain-json-editor` and import it
-// import { PlainJsonEditor } from 'plain-json-editor'
+// you would do `npm i react-plain-json-editor` and import it
+// import { PlainJsonEditor } from 'react-plain-json-editor'
 
 const App = () => {
 	const [style, setStyle] = useState<{}>({
